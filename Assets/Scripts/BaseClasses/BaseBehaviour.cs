b5badbffb4df2a232709bf7d53f51dd3d14854fa
Nolimet <nolimet@stamdata.nl>
2016-01-06 20:52:52 +0100

    commit init

---
+++ Assets/Scripts/BaseClasses/BaseBehaviour.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseBehaviour : MonoBehaviour
+{
+
+    //MainThreadDeltaTime;
+    System.DateTime t;
+    protected System.TimeSpan deltaTimeMain;
+
+    //SecondaryThreadDeltaTime;
+    System.DateTime t2;
+    protected System.TimeSpan deltaTimeSecondary;
+
+    private bool startedSecondThread;
+
+    protected virtual void Start()
+    {
+        startMainThreadUpdate();
+    }
+
+    protected void CalcDeltaTimeMain()
+    {
+        deltaTimeMain = (t - System.DateTime.Now);
+        t = System.DateTime.Now;
+    }
+
+    protected void CalcDeltaTimeSecondary()
+    {
+        deltaTimeSecondary = (t2 - System.DateTime.Now);
+        t2 = System.DateTime.Now;
+    }
+
+    protected void startMainThreadUpdate()
+    {
+        ScriptManager.registerScriptMainThread(this);
+    }
+
+    protected void StartSecondThreadUpdate()
+    {
+        ScriptManager.registerScriptSecondaryThread(this);
+        startedSecondThread = true;
+    }
+
+    protected void OnDestroy()
+    {
+        if (!Application.isPlaying)
+            return;
+
+        ScriptManager.unregisterScriptMainThread(this);
+        if (startedSecondThread)
+            ScriptManager.unregisterScriptSecondaryThread(this);
+    }
+
+    /// <summary>
+    /// runs in the main thread of unity
+    /// </summary>
+    public virtual void MainUpdate()
+    {
+
+    }
+
+    /// <summary>
+    /// runs in a self managed thread so out side the main thread of unity
+    /// </summary>
+    public virtual void SecondaryThreadUpdate()
+    {
+
+    }
+}
---
+++ Assets/Scripts/GameScripts/Mechanics/CountDownTimer.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+namespace game.mechanics
+{
+    public class CountDownTimer : BaseBehaviour
+    {
+        [Tooltip("in seconds")]
+        public int timerLenght;
+        System.DateTime counter;
+
+        public Text visualDisplay;
+
+        bool timerDone;
+
+        protected override void Start()
+        {
+            counter = System.DateTime.Now;
+            startMainThreadUpdate();
+            StartSecondThreadUpdate();
+        }
+
+        public override void MainUpdate()
+        {
+            visualDisplay.text = formatCounter();
+        }
+
+        public override void SecondaryThreadUpdate()
+        {
+            if ((System.DateTime.Now - counter).TotalSeconds > timerLenght)
+                timerDone = true;
+        }
+
+        System.TimeSpan tm;
+        string formatCounter()
+        {
+            tm = (new System.TimeSpan(0, 0, timerLenght) - (System.DateTime.Now - counter));
+            string output = "";
+            if (tm.Hours > 0)
+            {
+                if (tm.Hours < 10)
+                    output += "0";
+
+                output += tm.Hours;
+                output += " : ";
+            }
+
+            if (tm.Minutes > 0)
+            {
+                if (tm.Minutes < 10)
+                    output += "0";
+
+                output += tm.Minutes;
+                output += " : ";
+            }
+
+            if (tm.Seconds > 0)
+            {
+                if (tm.Seconds < 10)
+                    output += "0";
+
+                output += tm.Seconds;
+            }
+
+
+            return output;
+        }
+    }
+}
